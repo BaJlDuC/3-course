d5a6c146b0792d275f57301f72d80a268dc5f3e5
BaJlDuC <32042764+BaJlDuC@users.noreply.github.com>
2020-01-23 17:20:04 +0300

    fixed and added drawing

--- stateMachines/lab1/automatToOtherAutomat/automatToOtherAutomat/automatToOtherAutomat.cpp
+++ stateMachines/lab1/automatToOtherAutomat/automatToOtherAutomat/automatToOtherAutomat.cpp
@@ -15,6 +15,123 @@ const string MEALY_VERTEX_CHAR = "Z";
 const string MOORE_AUTOMAT = "moore";
 const string MEALY_AUTOMAT = "mealy";
 
+void createMooreAutomat(int inputCharsCount, int vertexCount, ifstream &inputFile, ofstream &outputAutomat, string** outputTable)
+{
+	map<string, string> states;
+	MooreMachine mooreMachine;
+	vector<string> vertexes;
+	string vertexValue;
+	cout << "automat: mealy" << endl;
+	for (int i = 0; i < inputCharsCount; i++) //заполняем таблицу c исходными состояниями (Мили)
+	{
+		for (int j = 0; j < vertexCount; j++)
+		{
+			inputFile >> vertexValue;
+			outputTable[i][j] = vertexValue;
+			vertexes.push_back(vertexValue);
+		}
+	}
+
+	map<string, string>::iterator it = states.begin();
+	int newValue = 0;
+	for (int i = 0; i < inputCharsCount; i++)
+	{
+		for (int j = 0; j < vertexCount; j++)
+		{
+			it = states.find(outputTable[i][j]);
+			if (it == states.end())
+			{
+				states.insert(make_pair(outputTable[i][j], MEALY_VERTEX_CHAR + to_string(newValue)));
+				newValue++;
+			}
+		}
+	}
+	//
+	it = states.begin();
+	for (int i = 0; it != states.end(); it++, i++) {
+		it->second = MEALY_VERTEX_CHAR + to_string(i);
+		mooreMachine.inputs.push_back(stoi(it->first.substr(1, 1)));
+		mooreMachine.outputs.push_back(it->first.substr(2, 3));
+	}
+	it = states.begin();
+	for (int i = 0; it != states.end(); it++, i++) {
+		cout << i << ") Key " << it->first << ", value " << it->second << endl;
+	}
+	//
+	int index = 0;
+
+	for (int i = 0; i < inputCharsCount + 1; i++)
+	{
+		it = states.begin();
+		if (i != 0)
+		{
+			mooreMachine.graph.push_back({});
+		}
+		for (int j = 0; j < states.size(); j++)
+		{
+			if (i == 0)
+			{
+				outputAutomat << it->first.substr(2) << ' ';
+				it++;
+			}
+			else if (i == 1)
+			{
+				index = stoi(it->first.substr(1, 1));
+				outputAutomat << states[vertexes.at(index)] << ' ';
+				mooreMachine.graph[0].push_back(stoi(states[vertexes.at(index)].substr(1, 1)));
+				it++;
+			}
+			else
+			{
+				index = stoi(it->first.substr(1, 1));
+				outputAutomat << states[vertexes.at(index + vertexCount * (i - 1))] << ' ';
+				mooreMachine.graph[i - 1].push_back(stoi(states[vertexes.at(index + vertexCount * (i - 1))].substr(1, 1)));
+				it++;
+			}
+		}
+		outputAutomat << endl;
+	}
+	CreateMooreGraph(mooreMachine, states);
+}
+void createMealyAutomat(int inputCharsCount, int vertexCount, ifstream &inputFile, ofstream &outputAutomat, string** outputTable)
+{
+	string vertexValue;
+	string outputChar;
+	string inputChar;
+	map<string, string> states;
+	MealyMachine mealyMachine;
+	cout << "automat: moore" << endl; //добавляем вершины без выходных символов
+	for (int i = 0; i < vertexCount; i++)
+	{
+		vertexValue = MOORE_VERTEX_CHAR;
+		vertexValue += to_string(i);
+		states.insert(make_pair(vertexValue, ""));
+	}
+
+	map<string, string>::iterator it = states.begin();
+	for (int i = 0; i < vertexCount; i++, it++) // добавляем выходные символы к вершинам
+	{
+		inputFile >> outputChar;
+		it->second = outputChar;
+	}
+
+	for (int i = 0; i < inputCharsCount; i++) //заполняем выход
+	{
+		mealyMachine.graph.push_back({});
+		for (int j = 0; j < vertexCount; j++)
+		{
+			inputFile >> inputChar;
+			it = states.find(inputChar);
+			outputTable[i][j] = it->first + it->second;
+			mealyMachine.graph[i].push_back(outputTable[i][j]);
+			outputAutomat << outputTable[i][j] << ' ';
+		}
+		outputAutomat << endl;
+	}
+	CreateMealyGraph(mealyMachine, states);
+}
+
+
 int main(int argc, char* argv[])
 {
 	if (argc != 3)
@@ -41,104 +158,13 @@ int main(int argc, char* argv[])
 		outputTable[i] = new string[vertexCount];
 	}
 
-	map<string, string> states;
-
 	if (automat == MOORE_AUTOMAT)
 	{
-		cout << "automat: moore" << endl; //добавляем вершины без выходных символов
-		for (int i = 0; i < vertexCount; i++)
-		{
-			vertexValue = MOORE_VERTEX_CHAR;
-			vertexValue += to_string(i);
-			states.insert(make_pair(vertexValue, ""));
-		}
-
-		map<string, string>::iterator it = states.begin();
-		for (int i = 0; i < vertexCount; i++, it++) // добавляем выходные символы к вершинам
-		{
-			inputFile >> outputChar;
-			it->second = outputChar;
-		}
-
-		for (int i = 0; i < inputCharsCount; i++) //заполняем выход
-		{
-			for (int j = 0; j < vertexCount; j++)
-			{
-				inputFile >> inputChar;
-				it = states.find(inputChar);
-				outputTable[i][j] = it->first + it->second;
-				outputAutomat << outputTable[i][j] << ' ';
-			}
-			outputAutomat << endl;
-		}
+		createMealyAutomat(inputCharsCount, vertexCount, inputFile, outputAutomat, outputTable);
 	}
 	else if (automat == MEALY_AUTOMAT)
 	{
-		vector<string> vertexes;
-		cout << "automat: mealy" << endl;
-		for (int i = 0; i < inputCharsCount; i++) //заполняем таблицу c исходными состояниями (Мили)
-		{
-			for (int j = 0; j < vertexCount; j++)
-			{
-				inputFile >> vertexValue;
-				outputTable[i][j] = vertexValue;
-				vertexes.push_back(vertexValue);
-			}
-		}
-
-		map<string, string>::iterator it = states.begin();
-		int newValue = 0;
-		for (int i = 0; i < inputCharsCount; i++)
-		{
-			for (int j = 0; j < vertexCount; j++)
-			{
-				it = states.find(outputTable[i][j]);
-				if (it == states.end())
-				{
-					states.insert(make_pair(outputTable[i][j], MEALY_VERTEX_CHAR + to_string(newValue)));
-					newValue++;
-				}
-			}
-		}
-		//
-		it = states.begin();
-		for (int i = 0; it != states.end(); it++, i++) { 
-			it->second = MEALY_VERTEX_CHAR + to_string(i);
-		}
-		it = states.begin();
-		for (int i = 0; it != states.end(); it++, i++) {
-			cout << i << ") Key " << it->first << ", value " << it->second << endl;
-		}
-		//
-		int index = 0;
-
-		for (int i = 0; i < inputCharsCount + 1; i++)
-		{
-			it = states.begin();
-			for (int j = 0; j < states.size(); j++)
-			{
-				if (i == 0)
-				{
-					outputAutomat << it->first.substr(2) << ' ';
-					it++;
-				}
-				else if (i == 1)
-				{
-					index = stoi(it->first.substr(1, 1));
-					outputAutomat << states[vertexes.at(index)] << ' ';
-					it++;
-				}
-				else 
-				{
-					index = stoi(it->first.substr(1, 1));
-					outputAutomat << states[vertexes.at(index + vertexCount * (i - 1))] << ' ';
-					it++;
-				}
-			}
-			outputAutomat << endl;
-		}
-		MooreMachine mooreMachine;
-		CreateMooreGraph(mooreMachine, states);
+		createMooreAutomat(inputCharsCount, vertexCount, inputFile, outputAutomat, outputTable);
 	}
 	else
 	{
--- stateMachines/lab1/automatToOtherAutomat/automatToOtherAutomat/vizualization.h
+++ stateMachines/lab1/automatToOtherAutomat/automatToOtherAutomat/vizualization.h
@@ -12,11 +12,15 @@ using namespace std;
 
 struct MooreMachine
 {
-	vector<string> outputs{ "y1", "y1", "y2", "y2", "y3" };
-	vector<vector<int>> graph{
-		{ 3, 0, 0, 2, 2 },
-		{ 1, 4, 4, 0, 0 }
-	};
+	vector<int> inputs; 
+	vector<string> outputs;
+	vector<vector<int>> graph; 
+};
+
+struct MealyMachine
+{
+	vector<vector<string>> graph;
+	vector<string> states;
 };
 
 struct VertexProps
@@ -31,16 +35,17 @@ struct EdgeProps
 
 using Graph = boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS, VertexProps, EdgeProps>;
 
-void CreateMooreGraph(const MooreMachine& mooreMachine, const map<string, string> &automatStates)
+void CreateMooreGraph(MooreMachine& mooreMachine, map<string, string> &automatStates)
 {
 	Graph graph;
 	vector<Graph::vertex_descriptor> vertices;
-	map<string, string>::iterator it;// = automatStates.begin();
+	map<string, string>::iterator it = automatStates.begin();
 	for (size_t i = 0; i < mooreMachine.graph[0].size(); ++i)
 	{
-		string vertexLabel = 'q' + to_string(i) + mooreMachine.outputs[i];
+		string vertexLabel = 'q' + to_string(mooreMachine.inputs.at(i)) + mooreMachine.outputs[i];
 		map<string, string>::iterator it = automatStates.find(vertexLabel);
-		vertexLabel += '-' + it->second;
+		//vertexLabel += '-' + it->second;
+		vertexLabel = it->second + (mooreMachine.outputs[i]);
 		vertices.push_back(boost::add_vertex({ vertexLabel }, graph));
 	}
 
@@ -57,6 +62,36 @@ void CreateMooreGraph(const MooreMachine& mooreMachine, const map<string, string
 	dp.property("label", boost::get(&VertexProps::label, graph));
 	dp.property("label", boost::get(&EdgeProps::label, graph));
 	dp.property("node_id", boost::get(boost::vertex_index, graph));
-	ofstream ofs("graph.dot");
+	ofstream ofs("moore.dot");
+	boost::write_graphviz_dp(ofs, graph, dp);
+
+}
+
+void CreateMealyGraph(MealyMachine& mealyMachine, map<string, string> &automatStates)
+{
+	Graph graph;
+	vector<Graph::vertex_descriptor> vertices;
+	map<string, string>::iterator it = automatStates.begin();
+	for (size_t i = 0; i < mealyMachine.graph[0].size(); ++i)
+	{
+		string vertexLabel = "q" + to_string(i);// +mooreMachine.outputs[i];
+		map<string, string>::iterator it = automatStates.find(vertexLabel);
+		vertices.push_back(boost::add_vertex({ vertexLabel }, graph));
+	}
+
+	for (size_t i = 0; i < mealyMachine.graph.size(); ++i)
+	{
+		for (size_t j = 0; j < mealyMachine.graph[0].size(); ++j)
+		{
+			string edgeLabel = 'x' + to_string(i + 1) + "/" + mealyMachine.graph[i][j].substr(2, 3);
+			boost::add_edge(vertices[j], vertices[stoi(mealyMachine.graph[i][j].substr(1, 1))], { edgeLabel }, graph);
+		}
+	}
+
+	boost::dynamic_properties dp;
+	dp.property("label", boost::get(&VertexProps::label, graph));
+	dp.property("label", boost::get(&EdgeProps::label, graph));
+	dp.property("node_id", boost::get(boost::vertex_index, graph));
+	ofstream ofs("mealy.dot");
 	boost::write_graphviz_dp(ofs, graph, dp);
 }
