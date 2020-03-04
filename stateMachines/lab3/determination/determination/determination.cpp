433ee97bc89daa717507dcef56a90d5bfe8916cb
BaJlDuC <32042764+BaJlDuC@users.noreply.github.com>
2020-03-04 14:35:56 +0300

    add determination

---
+++ stateMachines/lab3/determination/determination/determination.cpp
@@ -0,0 +1,11 @@
+﻿//3 программа по ТАиФЯ, Садовин Владислав, ПС-32 | Microsoft Visual Studio 2017
+#include "pch.h"
+#include "functions.h"
+
+int main(int argc, char* argv[])
+{
+	NondetMachine nondetMachine = ReadNondetMachine(argv[1]);
+	DetMachine detMachine = DetermineMachine(nondetMachine);
+	PrintDetMachine(detMachine, argv[2]);
+	CreateGraph(detMachine);
+}
